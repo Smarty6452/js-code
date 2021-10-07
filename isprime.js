c3401b5dd0b953fd575b062cfb1f426ddc017945
Smarty <rohitbharti326452@gmail.com>
2021-10-07 16:16:06 +0530

    initial-commit

---
+++ isprime.js
@@ -0,0 +1,23 @@
+function isPrime(num) {     //logic to check give number is prime or not
+	for (let i = 2; i < num; i++) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+
+	return num !== 1 && num !== 0;
+}
+
+function sumOfPrime(num2) {
+    let i = 1;
+    let sum = 0;
+    while (i <= num2) {
+        if(isPrime(i)) {
+            sum +=i;
+        }
+        i++;
+    }
+    return sum;
+}
+
+sumOfPrime(50)
---
+++ sumArray.js
@@ -0,0 +1,15 @@
+function SumArray(array) {
+
+    let max = Math.max(array[0], array[1]);
+    let min = Math.min(array[0], array[1]);
+
+    let SUM = 0 ;
+    for(let i = min; i<= max; i++) {
+        SUM += i;
+    }
+return SUM;
+// console.log(SUM);
+    
+
+}
+SumArray([1, 10])
